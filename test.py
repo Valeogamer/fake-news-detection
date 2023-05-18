f0279badb99d37beecb0f925b9fd382965698ff2
Valentin <79332916+Valeogamer@users.noreply.github.com>
2023-05-19 00:09:41 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import zipfile
 import pandas as pd
-with zipfile.ZipFile('download_dataset.zip', 'r') as zip_ref:
+with zipfile.ZipFile('csv-files.zip', 'r') as zip_ref:
     zip_ref.extractall('data')
-data_fake = pd.read_csv('data/Fake.csv')
-data_true = pd.read_csv('data/True.csv')
+data_fake = pd.read_csv('Fake.csv')
+data_true = pd.read_csv('True.csv')
 print(data_fake.head())
