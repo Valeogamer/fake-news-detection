db341ced95b60d2f9e81a7fb3a8c0ea9eeb1e3f1
Valentin <79332916+Valeogamer@users.noreply.github.com>
2023-05-19 21:56:35 +0300

    Update learn_and_save.py

--- learn_and_save.py
+++ learn_and_save.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import pickle
-import os
-import re
-import string
-from datetime import datetime
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorization = TfidfVectorizer()
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
-import joblib
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
 
 models_info = {}
 
@@ -18,57 +12,16 @@ def save_info(model_info):
           file.write(f"{key}: {value}\n")
   print("Сохранение: info.txt")
 
-def wordopt(text):
-  """
-    Удаление с текста всякого мусора
-  """
-  text = text.lower()
-  text = re.sub('\[.*?]', '', text)
-  text = re.sub("\\W", " ", text)
-  text = re.sub('https?://\S+|www\.\S+', '', text)
-  text = re.sub('<.*?>+', '', text)
-  text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-  text = re.sub('\n', '', text)
-  text = re.sub('\w*\d\w*', '', text)
-  return text
- 
-def preproces_data(data_fake, data_true):
-  print("Предобработка!")
-  data_fake['class'] = 0
-  data_true['class'] = 1
-  print("Объединение данных")
-  data_merge = pd.concat([data_fake, data_true], axis = 0)
-  print("Удаление не нужных колонок")
-  data_merge = data_merge.drop(['title', 'subject', 'date'], axis = 1)
-  print("Смешивание данных")
-  data_merge = data_merge.sample(frac = 1) # cмешивание данных
-  print("Переиндексация")
-  data_merge.reset_index(inplace = True)
-  data_merge.drop(['index'], axis=1, inplace=True)
-  return data_merge
+print("Загрузка необходимых данных")
+with open('xv_train.pickle', 'rb') as f:
+    xv_train = pickle.load(f)
 
-def spliter_learn(data, flag=None): 
-  print("Разделение на тестовые и обучающие")
-  x = data['text']
-  y = data['class']
-  X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.4)
-#   X_train.to_csv('X_train.csv')
-#   X_test.to_csv('X_test.csv')
-#   Y_train.to_csv('Y_train.csv')
-#   Y_test.to_csv('Y_test.csv')
-  if flag:
-    return X_train, X_test, Y_train, Y_test
+with open('xv_test.pickle', 'rb') as f:
+    xv_test = pickle.load(f)
 
-data_fake = pd.read_csv('Fake_new.csv')
-data_true = pd.read_csv('True_new.csv')
-# предобработка основных данных
-merged_data = preproces_data(data_fake, data_true)
-# Удаление мусора
-merged_data['text'] = merged_data['text'].apply(wordopt)
-# Разделение на обучающую и тестовую
-X_train, X_test, Y_train, Y_test = spliter_learn(merged_data, True)
-xv_train = vectorization.fit_transform(X_train)
-xv_test = vectorization.transform(X_test)
+Y_train = pd.read_csv('Y_train.csv')
+Y_test = pd.read_csv('Y_test.csv')
+merged_data_true_fake = pd.read_csv('merged.csv')
 
 print('-----Модели-----')
 print("---LogisticRegression---")
