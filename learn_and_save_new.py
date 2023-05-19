5a943b8a725de2ff1078a1d7471b590c3c564883
Valentin <79332916+Valeogamer@users.noreply.github.com>
2023-05-19 22:43:31 +0300

    Update learn_and_save_new.py

--- learn_and_save_new.py
+++ learn_and_save_new.py
@@ -1,9 +1,11 @@
 import pandas as pd
-import pickle
-import re
 import string
+import re
 from sklearn.metrics import classification_report
+# import joblib
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorization = TfidfVectorizer()
 
 models_info = {}
 
@@ -11,30 +13,10 @@ def save_info(model_info):
   with open('info.txt', "w") as file:
       for key, value in model_info.items():
           file.write(f"{key}: {value}\n")
-  print("Сохранение: info.txt")
-
-def compound():
-  print("Объединение")
-  data_fake_orig = pd.read_csv('Fake_new.csv')
-  data_fake_add = pd.read_csv('fake_add.csv')
-  fake = pd.concat([data_fake_orig, data_fake_add], axis=0)
-  fake.to_csv('Fake.csv', index=False)
-  data_true_orig = pd.read_csv('True_new.csv')
-  data_true_add = pd.read_csv('true_add.csv')
-  true = pd.concat([data_true_orig, data_true_add], axis=0)
-  true.to_csv('True.csv', index=False)
-
-def spliter_learn(data, test_size=0.25): 
-  print("Разделение на тестовые и обучающие")
-  x = data['text']
-  y = data['class']
-  X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size)
-  return X_train, X_test, Y_train, Y_test
-
+  print("Сохранение: new_info.txt")
+  
 def preproces_data(data_fake, data_true):
   print("Предобработка!")
-  data_fake['class'] = 0
-  data_true['class'] = 1
   print("Объединение данных")
   data_merge = pd.concat([data_fake, data_true], axis = 0)
   print("Удаление не нужных колонок")
@@ -47,9 +29,6 @@ def preproces_data(data_fake, data_true):
   return data_merge
 
 def wordopt(text):
-  """
-    Удаление с текста всякого мусора
-  """
   text = text.lower()
   text = re.sub('\[.*?]', '', text)
   text = re.sub("\\W", " ", text)
@@ -60,24 +39,31 @@ def wordopt(text):
   text = re.sub('\w*\d\w*', '', text)
   return text
 
-print("Загрузка необходимых данных")
-with open('xv_train.pickle', 'rb') as f:
-    xv_train = pickle.load(f)
+def compound():
+  print("Объединение")
+  data_fake_orig = pd.read_csv('Fake_new.csv')
+  data_fake_add = pd.read_csv('fake_add.csv')
+  fake = pd.concat([data_fake_orig, data_fake_add], axis=0)
+  fake.to_csv('Fake.csv', index=False)
+  data_true_orig = pd.read_csv('True_new.csv')
+  data_true_add = pd.read_csv('true_add.csv')
+  true = pd.concat([data_true_orig, data_true_add], axis=0)
+  true.to_csv('True.csv', index=False)
 
-with open('xv_test.pickle', 'rb') as f:
-    xv_test = pickle.load(f)
+compound()
 
-data_fake = pd.read_csv('Fake.csv')
-data_true = pd.read_csv('True.csv')
+data_f = pd.read_csv('Fake.csv')
+data_t = pd.read_csv('True.csv')
 
-# предобработка основных данных
-merged_data = preproces_data(data_fake, data_true)
-# Удаление мусора
-merged_data['text'] = merged_data['text'].apply(wordopt)
-# Разделение на обучающую и тестовую
-X_train, X_test, Y_train, Y_test = spliter_learn(merged_data, test_size=0.40)
+data = preproces_data(data_f, data_t)
+data['text'] = data['text'].apply(wordopt)
+x = data['text']
+y = data['class']
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.25)
 
-merged_data_true_fake = pd.read_csv('merged.csv')
+
+xv_train = vectorization.fit_transform(X_train)
+xv_test = vectorization.transform(X_test)
 
 print('-----Модели-----')
 print("---LogisticRegression---")
@@ -93,7 +79,7 @@ LR.score(xv_test, Y_test)
 print(classification_report(Y_test, pred_lr))
 models_info['LR time:'] = result_time
 models_info['LR CR: '] = classification_report(Y_test, pred_lr)
-
+# joblib.dump(LR, 'model_lr.joblib')
 
 print("---DecisionTreeClassifier---")
 from sklearn.tree import DecisionTreeClassifier
@@ -108,7 +94,7 @@ DT.score(xv_test, Y_test)
 print(classification_report(Y_test, pred_dt))
 models_info['DT time:'] = result_time
 models_info['DT CR: '] = classification_report(Y_test, pred_lr)
-
+# joblib.dump(DT, 'model_dt.joblib')
 
 print("---GradientBoostingClassifier---")
 from sklearn.ensemble import GradientBoostingClassifier
@@ -123,7 +109,7 @@ GB.score(xv_test, Y_test)
 print(classification_report(Y_test, pred_gb))
 models_info['GB time:'] = result_time
 models_info['GB CR: '] = classification_report(Y_test, pred_lr)
-
+# joblib.dump(GB, 'model_gb.joblib')
 
 print("---RandomForestClassifier---")
 from sklearn.ensemble import RandomForestClassifier
@@ -138,5 +124,6 @@ RF.score(xv_test, Y_test)
 print(classification_report(Y_test, pred_rf))
 models_info['RF time:'] = result_time
 models_info['RF CR: '] = classification_report(Y_test, pred_lr)
+# joblib.dump(RF, 'model_rf.joblib')
 
 save_info(models_info)
