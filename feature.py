236d1fa538ee7b52645cf5dfdf1e34d64b4b8ef0
Valentin <79332916+Valeogamer@users.noreply.github.com>
2023-05-20 07:22:43 +0300

    Update feature.py

--- feature.py
+++ feature.py
@@ -4,5 +4,4 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 vectorization = TfidfVectorizer()
 X_train = pd.read_csv('X_train.csv')
 xv_train = vectorization.fit_transform(X_train)
-xv_train, xv_test = vectoriz(X_test, X_train)
 joblib.dump(vectorization, 'vectorizer.joblib')
