da01628847e466563433b2e2ffb289236db1eed7
Valentin <79332916+Valeogamer@users.noreply.github.com>
2023-05-24 02:15:37 +0300

    Update feature.py

--- feature.py
+++ feature.py
@@ -4,10 +4,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 main_data = pd.read_csv('main_data.csv')
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(main_data['text'])
-with open('vectorizer.pickle', 'wb') as handle:
-    pickle.dump(vectorizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
-with open('features.pickle', 'wb') as handle:
-    pickle.dump(X, handle, protocol=pickle.HIGHEST_PROTOCOL)
+main_data_new = pd.read_csv('main_data_new.csv')
+def vec(main_data, name):
+  vectorizer = TfidfVectorizer()
+  X = vectorizer.fit_transform(main_data['text'])
+  with open(f'vectorizer_{name}.pickle', 'wb') as handle:
+      pickle.dump(vectorizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+      
+  with open(f'features_{name}.pickle', 'wb') as handle:
+      pickle.dump(X, handle, protocol=pickle.HIGHEST_PROTOCOL)
+old = 'old'
+new = 'new'
+vec(main_data, old)
+vec(main_data_new, new)
